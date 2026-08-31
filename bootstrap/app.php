a476c0aed4a217a3677fcaf1958c326a73b974db
plampambi-ui <plampambi@gmail.com>
2026-08-31 00:43:51 -0600

    Remove destructive scheduled migration and restrictive local host configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,6 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Console\Scheduling\Schedule;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,10 +12,6 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->trustProxies(at: '*');
-        $middleware->trustHosts(at: ['slippy-surf-wtnb61jj78.ploi.site']);
-    })
-    ->withSchedule(function (Schedule $schedule) {
-        $schedule->call('migrate:fresh --seed --force')->daily();
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
