829cd21d09c0ca0bc6e5f8def396def45afb1522
plampambi-ui <plampambi@gmail.com>
2026-08-30 22:42:39 -0600

    Add standalone PHP database configuration template

---
+++ plain-php/config.example.php
@@ -0,0 +1,7 @@
+<?php
+$CB_DB = [
+    'host' => 'localhost',
+    'name' => 'YOUR_HOSTINGER_DATABASE',
+    'user' => 'YOUR_HOSTINGER_DATABASE_USER',
+    'pass' => 'YOUR_HOSTINGER_DATABASE_PASSWORD',
+];
